e850029403f188f2b05cac483cd655615db0f348
pwwang <1188067+pwwang@users.noreply.github.com>
2018-07-09 13:49:09 -0700

    Update fish_greeting.fish

--- fish_greeting.fish
+++ fish_greeting.fish
@@ -2,7 +2,7 @@ function fish_greeting -d "What's up, fish?"
   set_color $fish_color_autosuggestion
   uname -nmsr | string trim
   # uptime not available in cygwin
-  if test type -q uptime
+  if type -q uptime
     uptime | string trim
   end
   set_color normal
